1633a948dbef5f3e8bd282ac74f7e265fb035e0d
Saiful182 <saifulislamcse0@gamil.com>
2023-01-29 21:50:34 +0600

    case funationality toatally done

--- JS/case.js
+++ JS/case.js
@@ -1,9 +1,19 @@
 
+
 document.getElementById('case-plus').addEventListener('click', function () {
     const priviousCaseNumberString = document.getElementById('case-number');
     const priviousCaseNumber = priviousCaseNumberString.value;
     const curentCaseNumber = parseInt(priviousCaseNumber) + 1;
     priviousCaseNumberString.value = curentCaseNumber;
+
+    // total case price
+
+    const priviousCasePriceString = document.getElementById('case-price');
+    const priviousCasePrice = parseFloat(priviousCasePriceString.innerText);
+
+    const newCasePrice = priviousCasePrice + 59;
+    priviousCasePriceString.innerText = newCasePrice;
+
 })
 
 document.getElementById('case-minus').addEventListener('click', function () {
@@ -11,4 +21,10 @@ document.getElementById('case-minus').addEventListener('click', function () {
     const priviousCaseNumber = priviousCaseNumberString.value;
     const curentCaseNumber = parseInt(priviousCaseNumber) - 1;
     priviousCaseNumberString.value = curentCaseNumber;
+
+    // total case price minus
+    const priviousCasePriceString = document.getElementById('case-price');
+    const priviousCasePrice = parseFloat(priviousCasePriceString.innerText);
+    const newCasePrice = priviousCasePrice - 59;
+    priviousCasePriceString.innerText = newCasePrice;
 })
